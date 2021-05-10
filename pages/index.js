a18d433cda889b58599637b33482d8225d9b18b6
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2021-05-11 08:51:58 +0900

    Fix API path

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 import { API_URL } from "@/config/index";
 
 export default function Home({ data }) {
-  return <div>{data.title}</div>;
+  return (
+    <>
+      <div>{data.title}</div>
+      <div>{data.body}</div>
+    </>
+  );
 }
 
 export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/api`);
+  const res = await fetch(`${API_URL}/api/v1/posts`);
   const data = await res.json();
 
   return {

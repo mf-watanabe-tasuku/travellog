078528dca2812a6a6567e08150f1742cadb21a4e
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2021-05-12 07:19:14 +0900

    Add Footer component

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function Layout({ title, keywords, description, children }) {
   return (
@@ -12,6 +13,7 @@ export default function Layout({ title, keywords, description, children }) {
 
       <Header />
       <div>{children}</div>
+      <Footer />
     </div>
   );
 }

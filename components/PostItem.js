8ba79af9e343009ce763ea5d545489832fee5668
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2021-05-13 07:26:13 +0900

    Add sample images to PostItem

--- components/PostItem.js
+++ components/PostItem.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function PostItem({ post }) {
   return (
     <div>
-      <Image src={"/images/sample2.jpg"} width={500} height={300} />
+      <Image src={`/images/sample${post.id}.jpg`} width={500} height={300} />
       <h2>{post.title}</h2>
       <p>{post.body}</p>
       <Link href={`/posts/${post.id}`}>

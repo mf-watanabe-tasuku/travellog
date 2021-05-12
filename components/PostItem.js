b1dcde4bc68d36e0c2359c9db85f50f1e7dd6a82
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2021-05-13 06:47:29 +0900

    Add thumbnail image to PostItem

--- components/PostItem.js
+++ components/PostItem.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function PostItem({ post }) {
   return (
     <div>
+      <Image src={"/images/sample2.jpg"} width={500} height={300} />
       <h2>{post.title}</h2>
       <p>{post.body}</p>
       <Link href={`/posts/${post.id}`}>

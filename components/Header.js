b9a23a31a389cd429b30b9d2058ccefa5910dd24
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2021-05-19 09:25:59 +0900

    Add Login and Register pages

--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,34 @@
+import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 import Link from "next/link";
 import styles from "@/styles/Header.module.css";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.header__container}>
-        <Link href="/">
-          <a className={styles.header__link}>Travel Log</a>
-        </Link>
-        <nav className={styles.nav}>
-          <ul className={styles.nav__list}>
-            <li>
-              <Link href="/posts/add">
-                <a className={styles.nav__link}>Add Post</a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <Link href="/">
+        <a className={styles.logo}>Travel Log</a>
+      </Link>
+      <nav className={styles.nav}>
+        <ul className={styles.nav__list}>
+          <li>
+            <Link href="/posts">
+              <a>Posts</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/posts/add">
+              <a>Add Post</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/account/login">
+              <a className="btn btn-secondary btn-icon">
+                <FaSignInAlt /> Login
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
